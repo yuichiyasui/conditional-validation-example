2b34431749ac27158670a007f3c412816674f539
yuichiyasui <yassan.b.a.w@gmail.com>
2023-11-04 11:20:23 +0900

    Add error message

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,83 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { clsx } from "clsx";
+
+type Id = {
+  field: string;
+  errorMessage: string;
+};
+
+const idDict = {
+  type: {
+    field: "type-field",
+    errorMessage: "type-error",
+  },
+  color: {
+    field: "color-field",
+    errorMessage: "color-error",
+  },
+  message: {
+    field: "message-field",
+    errorMessage: "message-error",
+  },
+} as const satisfies Record<string, Id>;
+
+const needMessageOption = {
+  yes: "1",
+  no: "0",
+} as const;
+
+type FormValues = {
+  type: string;
+  color: string;
+  needMessage: (typeof needMessageOption)[keyof typeof needMessageOption];
+  message: string;
+};
+
 export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = () => {
+    alert("確認画面に遷移します");
+  };
+
+  const formData = watch();
+  const needMessage = formData.needMessage === needMessageOption.yes;
+
   return (
-    <div>
+    <>
       <header className="p-4">
         <h1 className="text-2xl font-bold">Conditional Validation Example</h1>
       </header>
 
       <main className="p-4">
-        <h2 className="font-bold text-xl mb-2">ギフトの設定</h2>
-        <form className="flex flex-col gap-y-2">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-y-2"
+        >
+          <h2 className="font-bold text-xl mb-2">ギフトの設定</h2>
           <div className="form-control">
-            <label htmlFor="type" className="label">
+            <label htmlFor={idDict.type.field} className="label">
               包装の種類
             </label>
             <select
-              id="type"
-              className="select select-bordered w-full max-w-xs"
+              {...register("type", {
+                required: "包装の種類を選択してください",
+              })}
+              id={idDict.type.field}
+              aria-describedby={idDict.type.errorMessage}
+              aria-required
+              className={clsx(
+                "select",
+                "select-bordered",
+                "w-full",
+                "max-w-xs",
+                errors.type && "select-error",
+              )}
             >
               <option value="" disabled selected>
                 選択してください
@@ -22,14 +85,30 @@ export default function App() {
               <option value="1">ラッピング</option>
               <option value="2">リボン</option>
             </select>
+            <p
+              id={idDict.type.errorMessage}
+              aria-live="assertive"
+              className="text-error text-sm empty:mt-0 mt-2"
+            >
+              {errors.type && errors.type.message}
+            </p>
           </div>
           <div className="form-control">
-            <label htmlFor="color" className="label">
+            <label htmlFor={idDict.color.field} className="label">
               色
             </label>
             <select
-              id="color"
-              className="select select-bordered w-full max-w-xs"
+              {...register("color", { required: "色を選択してください" })}
+              id={idDict.color.field}
+              aria-required
+              aria-describedby={idDict.color.errorMessage}
+              className={clsx(
+                "select",
+                "select-bordered",
+                "w-full",
+                "max-w-xs",
+                errors.color && "select-error",
+              )}
             >
               <option value="" disabled selected>
                 選択してください
@@ -38,35 +117,68 @@ export default function App() {
               <option value="2">青</option>
               <option value="3">黄</option>
             </select>
+            <p
+              id={idDict.color.errorMessage}
+              aria-live="assertive"
+              className="text-error text-sm empty:mt-0 mt-2"
+            >
+              {errors.color && errors.color.message}
+            </p>
           </div>
           <fieldset className="form-control">
             <legend className="label">メッセージカードを添付しますか？</legend>
             <div className="flex gap-x-4">
               <label className="flex items-center gap-x-2">
                 <input
+                  {...register("needMessage")}
                   type="radio"
-                  name="needMessage"
                   defaultChecked
+                  value={needMessageOption.no}
                   className="radio"
                 />
                 <span className="text-sm">いいえ</span>
               </label>
               <label className="flex items-center gap-x-2">
-                <input type="radio" name="needMessage" className="radio" />
+                <input
+                  {...register("needMessage")}
+                  type="radio"
+                  value={needMessageOption.yes}
+                  className="radio"
+                />
                 <span className="text-sm">はい</span>
               </label>
             </div>
           </fieldset>
-          <div className="form-control">
-            <label htmlFor="message" className="label">
-              メッセージ
-            </label>
-            <textarea
-              id="message"
-              placeholder="お誕生日おめでとう"
-              className="textarea textarea-bordered max-w-xs w-full"
-            ></textarea>
-          </div>
+          {needMessage && (
+            <div className="form-control">
+              <label htmlFor={idDict.message.field} className="label">
+                メッセージ
+              </label>
+              <textarea
+                {...register("message", {
+                  required: "メッセージを入力してください",
+                })}
+                id={idDict.message.field}
+                aria-required
+                aria-describedby={idDict.message.errorMessage}
+                placeholder="お誕生日おめでとう"
+                className={clsx(
+                  "textarea",
+                  "textarea-bordered",
+                  "max-w-xs",
+                  "w-full",
+                  errors.message && "textarea-error",
+                )}
+              ></textarea>
+              <p
+                id={idDict.message.errorMessage}
+                aria-live="assertive"
+                className="text-error text-sm empty:mt-0 mt-2"
+              >
+                {errors.message && errors.message.message}
+              </p>
+            </div>
+          )}
           <button
             type="submit"
             className="btn btn-primary w-min whitespace-nowrap mt-4"
@@ -74,7 +186,9 @@ export default function App() {
             内容を確認する
           </button>
         </form>
+
+        <p>{JSON.stringify(formData, null, 2)}</p>
       </main>
-    </div>
+    </>
   );
 }
